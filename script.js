7e228e670f3b7e8c09ee4b016ae96fb3f83f2998
redbirdfan <redbirdfan38@gmail.com>
2022-11-28 19:48:22 -0600

    first save

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// TODO: add code here
+window.addEventListener("load", function (){
+    fetch("https://handlers.eduction.launchcode.org/static/astronauts.json").then(function(response) {
+    return response.json()
+        
+
+}).then(function(json) {
+        let astronaut;
+        console.log(json)
+
+    
+     })
+});
