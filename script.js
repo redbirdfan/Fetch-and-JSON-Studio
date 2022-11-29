f38de9c28e38f61409eafe8cc02203455fc015e4
redbirdfan <redbirdfan38@gmail.com>
2022-11-28 20:44:29 -0600

    finished product

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-window.addEventListener("load", function (){
-    fetch("https://handlers.eduction.launchcode.org/static/astronauts.json").then(function(response) {
-    return response.json()
-        
+window.addEventListener('load', function (){
+    const container =document.getElementById('container');
+    const count = document.getElementById('count');
 
-}).then(function(json) {
-        let astronaut;
-        console.log(json)
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then
+    (function(response) {
+        response.json().then(function(data) {
+                console.log(data);
+                
+                data.sort(function(a, b) {
+                    return a.hoursInSpace < b.hoursInSpace ? 1 : -1;
+                });
 
-    
-     })
+            for (let i=0; i < data.length; i++) {
+                let astronaut = data[i]
+                let activeClass = 'class="green"';
+              container.innerHTML += ` 
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                            <li>Active: ${astronaut.active}</li>
+                            <li>Skills: ${astronaut.skills.join(" ,")}</li>
+                        </ul>
+                    </div>
+                    
+            <img class="avatar" src= "${astronaut.picture}">
+        </div>
+        `
+            }
+        });
+    })
 });
